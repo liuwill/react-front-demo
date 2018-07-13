6e50683ab1d70d4b8acf2bbb4fbb0772996b1520
liuwill <liuwei_will@qq.com>
2018-07-13 15:19:22 +0800

    upload success

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,24 +6,37 @@ import Button from 'pile-ui/dist/components/button'
 import Inputs from 'pile-ui/dist/components/inputs'
 import Driver from 'pile-ui/dist/components/driver'
 
+// import Dialog from 'pile-ui/dist/components/dialog'
+// const { Toast } = Dialog
+
 export default class Home extends Component {
 
   constructor(props) {
     super(props)
 
-    this.state = {
+    this.uploadData = {
       token: '',
       key: '',
+      // crc32: '',
+      // accept: '',
     }
   }
 
   componentDidMount() {
   }
 
+  handleInputToken = (e) => {
+    this.uploadData.token = e.target.value
+  }
+
+  handleInputKey = (e) => {
+    this.uploadData.key = e.target.value
+  }
+
   handleBeforeUpload = (file) => {
     console.log('beforeUpload', file.name)
-    if (!this.state.token || !this.state.key) {
-
+    if (!this.uploadData.token || !this.uploadData.key) {
+      alert('请先填写token和文件名')
       return false
     }
   }
@@ -39,8 +52,9 @@ export default class Home extends Component {
         console.log('onStart', file.name)
         // this.refs.inner.abort(file)
       },
-      onSuccess(file) {
-        console.log('onSuccess', file)
+      onSuccess(file, data) {
+        alert('文件上传成功，具体信息见控制台')
+        console.log('onSuccess', file, data)
       },
       onProgress(step, file) {
         console.log('onProgress', Math.round(step.percent), file.name)
@@ -67,24 +81,27 @@ export default class Home extends Component {
         </div>
         <div className="page-form">
           <div className="page-form-item">
-            <Inputs placeholder="输入上传Token" mainValue={this.state.token} />
+            <Inputs placeholder="输入上传Token" onChange={this.handleInputToken} mainValue='' />
           </div>
           <div className="page-form-item">
-            <Inputs placeholder="输入bucket key" mainValue={this.state.key} />
+            <Inputs placeholder="输入bucket key" onChange={this.handleInputKey} mainValue='' />
           </div>
         </div>
         <Upload
           {...uploaderProps}
-          data={{
-            token: this.state.token,
-            key: this.state.key,
-            crc32: '',
-            accept: '',
-          }}
+          data={this.uploadData}
           component="div"
         >
           <Button>上传文件</Button>
         </Upload>
+
+        {/* <Toast
+          content='请先填写上传信息'
+          toastShow={true}
+          type='warning' // 类型：失败：fail，成功：success，错误：warning,loading : loading
+          time={3000}
+          callback={function () { console.log("状态更改时的触发") }}
+        /> */}
       </div>
     )
   }
